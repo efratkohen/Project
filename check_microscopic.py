d81897da68607a34502e856ffd0e9f697de154a5
Guy_Weintraub <guywine@gmail.com>
2020-07-26 16:59:56 +0300

    changes to files not project

--- check_microscopic.py
+++ check_microscopic.py
@@ -9,8 +9,10 @@ from project import *
 fname = "microscopic_data.csv"
 data_fname = check_file(fname)
 data = read_data(data_fname)
-data_m_reactor1, data_m_reactor2, data_m_reactor3, data_m_reactor4 = split_microscopic_to_reactor(data)
-data_m_reactor1.to_excel(r"E:\python\Microorganism_Effects_Analysis\export_data_m_reactor1.xlsx", index=False, header=True)
+convert_dates_to_date_object(data)
+micro_df_list = split_microscopic_to_reactor(data)
+# data_m_reactor1, data_m_reactor2, data_m_reactor3, data_m_reactor4 = split_microscopic_to_reactor(data)
+# data_m_reactor1.to_excel(r"E:\python\Microorganism_Effects_Analysis\export_data_m_reactor1.xlsx", index=False, header=True)
 # data.to_excel(r"E:\python\Microorganism_Effects_Analysis\export_dataframe_filter.xlsx", index=False, header=True)
 # reactor1_col = [data.columns[1:54]]
 # print(reactor1_col)
--- time_hyper_params.py
+++ time_hyper_params.py
@@ -21,27 +21,55 @@ def SVI_read_and_process():
     SV_label=[2.5, 3.0] 
     # define between bad, reasonable and good result for SV
 
-    fname = "SVI.csv"
-    data_fname = pr.check_file(fname)
+    svi_fname = "SVI.csv"
+    data_fname = pr.check_file(svi_fname)
     data_SVI = pr.read_data(data_fname)
+    pr.convert_dates_to_date_object(data_SVI)
     data_SVI = pr.check_SVI_values(data_SVI)
     data_SVI = data_SVI.interpolate()
     data_SVI_clean = pr.SVI_calculate(data_SVI)
-    data_reactor1, data_reactor2, data_reactor3, data_reactor4 = pr.split_SVI_to_reactor(data_SVI_clean)
-    bio_reactor_df_list = [data_reactor1, data_reactor2, data_reactor3, data_reactor4]
+
+    bio_reactor_df_list = pr.split_SVI_to_reactor(data_SVI_clean)
 
     for bio_reactor_df in bio_reactor_df_list:
         bio_reactor_df = pr.label_data(bio_reactor_df, SVI_label, SV_label)
     return bio_reactor_df_list
+
 
 def turn_str_to_date(date_s:str) -> datetime:
     '''
     Gets str like '04/08/2017 00:00'
     Removes hours and turns it to datetime object.
     '''
-    
+    pass
+
 
 
 if __name__=='__main__':
-    svi_df_list = SVI_read_and_process()
-    svi1 = svi_df_list[0]
+    # svi_df_list1 = SVI_read_and_process()
+    SVI_label=[190.0, 160.0]
+    # define between bad, reasonable and good result for SVI
+    SV_label=[2.5, 3.0] 
+    # define between bad, reasonable and good result for SV
+
+    svi_fname = "SVI.csv"
+    data_fname = pr.check_file(svi_fname)
+    data_SVI = pr.read_data(data_fname)
+    # x = pr.read_data(data_fname)
+    pr.convert_dates_to_date_object(data_SVI)
+
+    data_SVI = pr.check_SVI_values(data_SVI)
+
+    x = data_SVI.copy()
+
+    data_SVI.interpolate(inplace=True)
+
+    time_inter = data_SVI.interpolate(method='time')
+    # data_SVI_clean = pr.SVI_calculate(data_SVI)
+
+    # svi_df_list = pr.split_SVI_to_reactor(data_SVI_clean)
+
+    # for bio_reactor_df in svi_df_list:
+    #     bio_reactor_df = pr.label_data(bio_reactor_df, SVI_label, SV_label)
+    
+    # svi2 = svi_df_list[1]
