3f3d5d4e607312636f49632b160775c5274a153c
efrat kohen <efrat.kohen@gmail.com>
2020-07-26 16:43:10 +0300

    add microscopic_data_read_and_process + date converter

--- time_hyper_params.py
+++ time_hyper_params.py
@@ -10,6 +10,7 @@ def SVI_read_and_process():
     Computes SV and SVI.
     Splits to 4 bio reactors.
     Labels data by determined values.
+    Turns strings of 'date' columns to date objects.
 
     Return
     -------
@@ -32,7 +33,31 @@ def SVI_read_and_process():
 
     for bio_reactor_df in bio_reactor_df_list:
         bio_reactor_df = pr.label_data(bio_reactor_df, SVI_label, SV_label)
+        bio_reactor_df = pr.convert_dates_to_date_object(bio_reactor_df)
     return bio_reactor_df_list
+
+def microscopic_data_read_and_process():
+    '''
+    Uses helper function to process microscopic_data.csv.
+    Reads microscopic data sheet.
+    Splits to 4 bio reactors.
+    Turns strings of 'date' columns to date objects.
+
+    Return
+    -------
+    - bio_reactor_df_list: List of 4 dfs, each representing a bio_reactor
+    '''
+
+    fname = "microscopic_data.csv"
+    data_fname = pr.check_file(fname)
+    data_microscopic = pr.read_data(data_fname)
+    data_reactor1, data_reactor2, data_reactor3, data_reactor4 = pr.split_microscopic_to_reactor(data_microscopic)
+    bio_reactor_df_list = [data_reactor1, data_reactor2, data_reactor3, data_reactor4]
+
+    for bio_reactor_df in bio_reactor_df_list:
+        bio_reactor_df = pr.convert_dates_to_date_object(bio_reactor_df)
+    return bio_reactor_df_list
+
 
 def turn_str_to_date(date_s:str) -> datetime:
     '''
@@ -44,4 +69,8 @@ def turn_str_to_date(date_s:str) -> datetime:
 
 if __name__=='__main__':
     svi_df_list = SVI_read_and_process()
+    microscopic_df_list = microscopic_data_read_and_process()
     svi1 = svi_df_list[0]
+    microscopic1 = microscopic_df_list[0]
+    # svi1.to_excel(r"E:\python\Microorganism_Effects_Analysis\export_data_svi1.xlsx", index=False, header=True)
+    # microscopic1.to_excel(r"E:\python\Microorganism_Effects_Analysis\export_data_microscopic1.xlsx", index=False, header=True)
