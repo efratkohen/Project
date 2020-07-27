4e95c18c7009cf960a3876557a500c53340a789a
Guy_Weintraub <guywine@gmail.com>
2020-07-27 21:45:40 +0300

    correct converting to time

--- ml_prepare.py
+++ ml_prepare.py
@@ -3,3 +3,5 @@ import pandas as pd
 if __name__=='__main__':
     micro0 = pd.read_csv('clean_tables/micro_0.csv', index_col=False)
     svi_0 = pd.read_csv('clean_tables/svi_0.csv', index_col=False)
+
+    micro0.index = pd.to_datetime(micro0.index)
