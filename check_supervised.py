d0dcd8ef6e6d787b820b99052f117004b5fe3487
efrat kohen <efrat.kohen@gmail.com>
2020-07-27 12:00:07 +0300

    join data including hyper paramter

--- check_supervised.py
+++ check_supervised.py
@@ -11,13 +11,19 @@ if __name__=='__main__':
     svi_df_list = SVI_read_and_process()
     microscopic_df_list = microscopic_data_read_and_process()
     microscopic_df_list = drop_nan_rows(microscopic_df_list)
-    microscopic1 = microscopic_df_list[0]
-    #print(microscopic1.head)
+    join_data_1, join_data_2, join_data_3, join_data_4 = merge_data_df_list(19, 27, microscopic_df_list, svi_df_list, 3)
+    join_list = [join_data_1, join_data_2, join_data_3, join_data_4]
+    print(join_list[0])
 
+
+    # microscopic1 = microscopic_df_list[0]
     # svi1 = svi_df_list[0]
+    # join_df = merge_data(19, 27, microscopic1, svi1, 3)
+    # X = join_df.iloc[:, np.r_[2:9]]
+    # Y = join_df[['SV_label']]
+
+    
     # svi1.to_excel(r"E:\python\Microorganism_Effects_Analysis\export_svi1.xlsx", index=False, header=True)
     # microscopic1.to_excel(r"E:\python\Microorganism_Effects_Analysis\export_microscopic1.xlsx", index=False, header=True)
-    # join_data=svi1.set_index('date', drop=False).join(microscopic1.set_index('date'))
-    # join_data.to_excel(r"E:\python\Microorganism_Effects_Analysis\export_re1.xlsx", index=False, header=True)
-    # X = join_data.iloc[:, np.r_[33:41]]
-    # Y = join_data[['SV_label']]
+    #join_df.to_excel(r"E:\python\Microorganism_Effects_Analysis\export_re1.xlsx", index=False, header=True)
+  
