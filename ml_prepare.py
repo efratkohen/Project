9e05915eb21d0d3f3aa4257ff099095e16ef2e6c
Guy_Weintraub <guywine@gmail.com>
2020-07-27 21:46:21 +0300

    comment

--- ml_prepare.py
+++ ml_prepare.py
@@ -4,4 +4,5 @@ if __name__=='__main__':
     micro0 = pd.read_csv('clean_tables/micro_0.csv', index_col=False)
     svi_0 = pd.read_csv('clean_tables/svi_0.csv', index_col=False)
 
+    # now interpolation by time works
     micro0.index = pd.to_datetime(micro0.index)
