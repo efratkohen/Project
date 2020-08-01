ea259d4f9ac7a8f34dfc64828b930f02cb21f9a6
Guy_Weintraub <guywine@gmail.com>
2020-08-02 01:08:25 +0300

    updated docstrings

--- clean_data_svi.py
+++ clean_data_svi.py
@@ -1,9 +1,11 @@
-import pandas as pd 
+import pandas as pd
 import numpy as np
+
 
 def check_svi_values_range(svi_data: pd.DataFrame) -> pd.DataFrame:
     """
-    Check and replace incorrect values with nan
+    Check and replace incorrect values with nan.
+    (Zero values or values out of defined ranges)
 
     Parameters
     ----------
@@ -12,9 +14,7 @@ def check_svi_values_range(svi_data: pd.DataFrame) -> pd.DataFrame:
     return
     ----------
     svi_data: pd.DataFrame
-
     """
-
     svi_data.replace(0, np.nan, inplace=True)
     ranges_lst = [(0, 6), (0, 1000), (500, 4000)]
     col_names_lst = ["SV", "volume", "mlss"]
@@ -32,7 +32,7 @@ def check_svi_values_range(svi_data: pd.DataFrame) -> pd.DataFrame:
 
 def svi_calculate(data_svi: pd.DataFrame) -> pd.DataFrame:
     """
-    Add column of SVI caculation fot each reactor
+    Add column of SVI caculation to each reactor.
     
     Parameters
     ----------
@@ -43,57 +43,61 @@ def svi_calculate(data_svi: pd.DataFrame) -> pd.DataFrame:
     data_svi: pd.DataFrame
     """
     for i in range(1, 5):
-        data_svi.loc[:,f"SVI{i}"] = data_svi[f"volume reactor {i}"] * 1000 / data_svi[f"mlss reactor {i}"]
+        data_svi.loc[:, f"SVI{i}"] = (
+            data_svi[f"volume reactor {i}"] * 1000 / data_svi[f"mlss reactor {i}"]
+        )
 
     return data_svi
 
 
 def set_datetime_index(df_list: list):
     """
-    Set datetime as index.
+    Set 'date' column as datetime index.
+    Changes df inplace.
 
     Parameters
     ----------
     df_list: list
-    
     """
     for i in range(4):
-        df_list[i].set_index('date', inplace=True)
+        df_list[i].set_index("date", inplace=True)
         df_list[i].index = pd.to_datetime(df_list[i].index)
 
 
 def interpolate_svi_dfs(svi_df_list: list):
     """
-    Replace nan values with interpolate function.
+    Replace nan values using interpolate function, with method='time'
+    Changes df inplace.
 
     Parameters
     ----------
     svi_df_list: list
-    
     """
     for i in range(4):
-        svi_df_list[i].interpolate(inplace=True, method='time')
+        svi_df_list[i].interpolate(inplace=True, method="time")
 
 
 def svi_label(svi_df_list: list):
     """
-    Add labels columns in each dataframe in svi dataframe list
+    Adds label columns in each dataframe in svi dataframe list,
+    Using "label_data" function.
+    Added columns are: 'SV_label', 'SVI_label'.
     
     Parameters
     ----------
     svi_df_list: list
-
     """
     # define borders between bad / reasonable / good results
-    SVI_label=[160.0, 120.0]
-    SV_label=[3.0, 3.5] 
+    SVI_label = [160.0, 120.0]
+    SV_label = [3.0, 3.5]
     for svi_df in svi_df_list:
         svi_df = label_data(svi_df, SVI_label, SV_label)
 
 
 def label_data(data_svi: pd.DataFrame, label_SVI: list, label_SV: list) -> pd.DataFrame:
     """
-    Add labels column for SVI and SV results as bad, reasonable or good
+    Adds label columns 'SV_label' and 'SVI_label' to the dataframe.
+    Labels are 'bad', 'reasonable' or 'good'.
 
     Parameters
     ----------
@@ -106,15 +110,15 @@ def label_data(data_svi: pd.DataFrame, label_SVI: list, label_SV: list) -> pd.Da
     return
     ----------
     data_svi: pd.DataFrame
-
     """
-
-    data_svi.loc[:,"SV_label"] = np.where(
+    data_svi.loc[:, "SV_label"] = np.where(
         data_svi.loc[:, "Settling_velocity"] <= label_SV[0],
         "bad",
-        np.where(data_svi.loc[:, "Settling_velocity"] <= label_SV[1], "reasonable", "good"),
+        np.where(
+            data_svi.loc[:, "Settling_velocity"] <= label_SV[1], "reasonable", "good"
+        ),
     )
-    data_svi.loc[:,"SVI_label"] = np.where(
+    data_svi.loc[:, "SVI_label"] = np.where(
         data_svi.loc[:, "SVI"] >= label_SVI[0],
         "bad",
         np.where(data_svi.loc[:, "SVI"] >= label_SVI[1], "reasonable", "good"),
