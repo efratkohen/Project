7ce0f43e7515b5e1bca18ec1d737c5085aa7f0d2
Guy_Weintraub <guywine@gmail.com>
2020-08-01 10:59:26 +0300

    started writing tests names

---
+++ tests/test_clean_data_microscope.py
@@ -0,0 +1,4 @@
+from clean_data_microscope import *
+from files_process_save import *
+
+import pytest
--- tests/test_clean_data_svi.py
+++ tests/test_clean_data_svi.py
@@ -1,6 +1,37 @@
-
 from clean_data_svi import *
+from files_process_save import *
 
-import pathlib
 import pytest
+
+def test_check_svi_values_range_zeros():
+    pass
+
+def test_check_svi_values_range_ranges():
+    pass
+
+def test_svi_calculate_results():
+    pass
+
+def test_set_datetime_index():
+    pass
+
+def test_interpolate_svi_dfs_changed_list():
+    pass
+
+def test_svi_label_changed_list():
+    pass
+
+def test_label_data():
+
+
+
+
+
+
+
+
+
+
+
+
 
